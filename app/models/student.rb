1894bd80d3ab556e3f8982934cc7528b4a39d0e0
github username <github email address>
2018-07-09 10:53:47 +0100

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,24 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(term)
+    if term.present?
+      where('NAME like ?', "%#{term}%")
+    else
+      self.all
+    end
+  end
 end
+
+
+# search
+#     when it receives an empty search argument
+#       returns all students (FAILED - 1)
+#     when it receives a search query
+#       returns all students whose names contain the given string
+#
+# search
+#    when it receives a search query
+#      returns all students whose names contain the given string (FAILED - 7)
+#      renders the results with a partial (FAILED - 8)
